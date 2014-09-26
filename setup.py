0c1ed0f704af3ae7b3f7256d5733672a531abfe4
Brent Woodruff <brent@fprimex.com>
2014-09-26 15:52:40 -0400

    prep setup.py for 2.0.0 using Python3

--- setup.py
+++ setup.py
@@ -2,31 +2,31 @@ from setuptools import setup
 import sys
 
 extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
+if sys.version_info < (3,):
+    extra['use_3to2'] = True
 
 setup(
-	# Basic package information.
-	name = 'zdesk',
-	author = 'Brent Woodruff, Max Gutman, Stefan Tjarks',
-	version = '1.2.0',
-	author_email = 'brent@fprimex.com',
-	packages = ['zdesk'],
-	include_package_data = True,
-	install_requires = ['httplib2', 'simplejson'],
-	license='LICENSE.txt',
-	url = 'https://github.com/fprimex/zdesk',
-	keywords = 'zendesk api helpdesk',
-	description = 'Python API Wrapper for Zendesk',
-	classifiers = [
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Topic :: Internet',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 3',
-	],
+    # Basic package information.
+    name = 'zdesk',
+    author = 'Brent Woodruff',
+    version = '2.0.0',
+    author_email = 'brent@fprimex.com',
+    packages = ['zdesk'],
+    include_package_data = True,
+    install_requires = ['httplib2', 'simplejson'],
+    license='LICENSE.txt',
+    url = 'https://github.com/fprimex/zdesk',
+    keywords = 'zendesk api helpdesk',
+    description = 'Python API Wrapper for Zendesk',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
     **extra
 )
 
