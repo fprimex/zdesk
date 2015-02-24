45146f6d135270ade20a583019c96175bae86a76
Brent Woodruff <brent@fprimex.com>
2015-02-23 21:38:30 -0500

    bump version to 2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # Basic package information.
     name = 'zdesk',
     author = 'Brent Woodruff',
-    version = '2.1.0',
+    version = '2.1.1',
     author_email = 'brent@fprimex.com',
     packages = ['zdesk'],
     include_package_data = True,
